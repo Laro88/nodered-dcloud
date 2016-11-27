e0d734c2d476fa75f68fafb156c4b0dc6144682b
jesramsing <jesramsing@danfoss.com>
2016-11-27 22:38:54 +0100

    GraphSlicer added - note that it does not merge multiple sources yet

---
+++ DCloudGraphSlicer.js
@@ -0,0 +1,68 @@
+﻿module.exports = function (RED) {
+
+    var util = require('util');
+
+    function DCloudGraphSlicer(cfg) {
+        "use strict";
+        RED.nodes.createNode(this, cfg);
+        var node = this;
+        node.config = cfg;
+
+        //Handle input
+        this.on('input', function (msg) {
+            //this.log('input:' + util.inspect(msg)+ ' typeof:'  + typeof(msg));
+
+            try {
+                var data = msg.payload.data;
+                //this.log('data identified:' + util.inspect(data));
+                var g = {};
+
+                var lnk_varid_name = {}; //create inverse lookup having varids (when looping through data)
+                for (var ptr = 0; ptr < data.variablesRequested.length; ptr++) {
+                    var v = data.variablesRequested[ptr];
+                    g[v.name] = [];
+
+                    for (var ptrV = 0; ptrV < v.varids.length; ptrV++) {
+                        lnk_varid_name[v.varids[ptrV].varid] = v.name;
+                    }
+                }
+
+                //console.log('lnk:' + util.inspect(lnk_varid_name));
+
+                //loop through data
+                for (var ptr = 0; ptr < data.data.length; ptr++) {
+                    var d = data.data[ptr];
+
+                    var ts = Date.parse(data.sessions[d.sessionid].tsB);
+                    var val = d.val;
+                    var name = lnk_varid_name[d.varid];
+                    //console.log('name:' + name + ' val:' + val + ' varid:' + d.varid);
+                    if (g.hasOwnProperty(name) == true) {
+                        g[name].push([ts, val]);
+                    }
+                }
+
+                //filter - future, remove overloading data :-)
+
+                //build resulting graph arrays
+                var msgResult = {};
+                msgResult.topic = 'GraphSlicer';
+                msgResult.payload = [];
+                Object.keys(g).forEach(function (n) {
+                    var p = {};
+                    p.key = n;
+                    p.values = g[n];
+                    msgResult.payload.push(p);
+                });
+                //console.log('msgResult:' + util.inspect(msgResult));
+                node.send(msgResult);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+        
+    }
+    
+    RED.nodes.registerType("DCloudGraphSlicer", DCloudGraphSlicer);
+}
---
+++ test/utGraphSlicer.js
@@ -0,0 +1,10 @@
+﻿var assert = require('assert');
+
+describe('DCloudGraphSlicer', function() {
+    it('Simple', function() {
+        assert.ok(true, "This shouldn't fail");
+        var gs = require('../DCloudGraphSlicer.js');
+    })
+
+    
+})
